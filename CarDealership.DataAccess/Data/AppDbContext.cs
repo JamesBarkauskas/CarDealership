1e85f7bfc4b1457c36cbdf17f3b7e1712544a90f
James <jimbarkauskas@gmail.com>
2025-03-11 08:53:50 -0500

    Add project files.

---
+++ CarDealership.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CarDealership.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // add Tables to out Db
+        DbSet<CarMake> CarMakes { get; set; }
+
+        // seed data into our tables
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CarMake>().HasData(
+                new CarMake { Id = 1, Name = "Chevrolet" },
+                new CarMake { Id = 2, Name = "Ford" }
+                );
+        }
+    }
+}
---
+++ CarDealership.DataAccess/Migrations/20250311135253_AddCarMakeTableAndSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarDealership.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCarMakeTableAndSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarMakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarMakes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CarMakes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Chevrolet" },
+                    { 2, "Ford" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarMakes");
+        }
+    }
+}
