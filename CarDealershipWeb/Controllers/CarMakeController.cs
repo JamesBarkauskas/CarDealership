d60e383cc63a8d2de6ac101c482cfc048755ad2b
James <jimbarkauskas@gmail.com>
2025-03-17 14:24:43 -0500

    Added CarMake Controller

---
+++ CarDealershipWeb/Controllers/CarMakeController.cs
@@ -0,0 +1,90 @@
+﻿using CarDealership.DataAccess.Repository.IRepository;
+using CarDealership.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarDealershipWeb.Controllers
+{
+    public class CarMakeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CarMakeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<CarMake> carMakeList = _unitOfWork.CarMake.GetAll().ToList();
+            return View(carMakeList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new CarMake());
+            }
+            else
+            {
+                CarMake carMake = _unitOfWork.CarMake.Get(u => u.Id == id);
+                return View(carMake);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(CarMake carMake)
+        {
+            if (ModelState.IsValid)
+            {
+                if (carMake.Id == 0)
+                {
+                    _unitOfWork.CarMake.Add(carMake);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Car make added";
+                }
+                else
+                {
+                    _unitOfWork.CarMake.Update(carMake);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Car make updated";
+                }
+                
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // ModelState not valid...
+                return View();
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+            CarMake obj = _unitOfWork.CarMake.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+            CarMake carMake = _unitOfWork.CarMake.Get(u => u.Id == id);
+            if (carMake == null) { return NotFound(); }
+            _unitOfWork.CarMake.Remove(carMake);
+            _unitOfWork.Save();
+            TempData["success"] = "Car make removed";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- CarDealershipWeb/Program.cs
+++ CarDealershipWeb/Program.cs
@@ -1,4 +1,6 @@
 using CarDealership.DataAccess.Data;
+using CarDealership.DataAccess.Repository;
+using CarDealership.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,8 @@ builder.Services.AddControllersWithViews();
 // configure DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
